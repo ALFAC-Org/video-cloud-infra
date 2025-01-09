e168d4c8ca91a61beeb5869629e0780f5f359449
carlohcs <carlohcs@gmail.com>
2025-01-09 00:25:58 -0300

    fix: add AWS provider configuration and tags for S3 buckets in Terraform

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,9 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 terraform {
   backend "s3" {}
 }
 
 resource "aws_s3_bucket" "video" {
   bucket = "video-bucket"
+
+  tags = {
+    Name = "video-bucket"
+  }
 
   # Garante que irá destruir o bucket 
   # quando o comando terraform destroy for executado
@@ -15,6 +23,10 @@ resource "aws_s3_bucket" "video" {
 
 resource "aws_s3_bucket" "zip" {
   bucket = "zip-bucket"
+
+  tags = {
+    Name = "zip-bucket"
+  }
 
   # Garante que irá destruir o bucket 
   # quando o comando terraform destroy for executado
